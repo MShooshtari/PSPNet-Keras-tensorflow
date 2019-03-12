f89c745b4c62709a70f58039a2275e3fa87ff7cc
Mahdi Shooshtari <mshoosht@gra-login1.graham.sharcnet>
2019-03-12 12:42:15 -0400

    job requirement files added.

--- PSPNet_Sample_Job.sh
+++ PSPNet_Sample_Job.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=12800M        # memory per node
-#SBATCH --time=00:30:00
+#SBATCH --time=00:05:00
 #SBATCH --job-name=PSPNet
 #SBATCH --output=outputs/%x-%j.out
+
+######################## tttttSBATCH --gres=gpu:4
+module load nixpkgs/16.09 gcc/5.4.0 cuda/8.0.44 cudnn/6.0 opencv/3.3.0 python/3.6.3 bazel/0.5.2
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --upgrade pip
+
+pip install -r requirements.txt
+
 echo "Hello world"
-python pspnet.py -m pspnet101_cityscapes -i example_images/cityscapes.png -o example_results/cityscapes.jpg
+### python pspnet.py -m pspnet101_cityscapes -i example_images/cityscapes.png -o example_results/cityscapes.jpg
