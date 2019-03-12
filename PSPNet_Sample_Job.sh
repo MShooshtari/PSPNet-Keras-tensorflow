55c96a8b2b23fb1bdd233f5fae91d79bd12e3a1c
Mahdi Shooshtari <mshoosht@cedar5.cedar.computecanada.ca>
2019-03-12 11:40:35 -0700

    PSPNet is working properly on Cedar now...

--- PSPNet_Sample_Job.sh
+++ PSPNet_Sample_Job.sh
@@ -6,7 +6,7 @@
 #SBATCH --exclusive
 #SBATCH --output=outputs/%x-%j.out
 #SBATCH --gres=gpu:4
-module load nixpkgs/16.09 gcc/5.4.0 cuda/8.0.44 cudnn/6.0 opencv/3.3.0 python/3.6.3 bazel/0.5.2
+module load nixpkgs/16.09 gcc/5.4.0 cuda/8.0.44 cudnn/6.0 opencv/3.3.0 python/3.5.4 bazel/0.5.2
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --upgrade pip
@@ -14,4 +14,8 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Hello world"
-### python pspnet.py -m pspnet101_cityscapes -i example_images/cityscapes.png -o example_results/cityscapes.jpg
+python pspnet.py -m pspnet101_cityscapes -i example_images/Tree_ID_161640.png -o example_results/Tree_ID_161640.jpg
+python pspnet.py -m pspnet101_cityscapes -i example_images/Tree_ID_161639.png -o example_results/Tree_ID_161639.jpg
+python pspnet.py -m pspnet101_cityscapes -i example_images/Tree_ID_164660.png -o example_results/Tree_ID_164660.jpg
+python pspnet.py -m pspnet101_cityscapes -i example_images/Tree_ID_164659.png -o example_results/Tree_ID_164659.jpg
+python pspnet.py -m pspnet101_cityscapes -i example_images/Tree_ID_163639.png -o example_results/Tree_ID_163639.jpg
